d1233468d6e990ae55639d1a2b1eced53bca217d
Matthew Feng <mattfeng@mit.edu>
2021-10-13 02:02:21 -0400

    lots more about pcr

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,56 +1,24 @@
 import * as React from "react"
-
 import { Link } from "gatsby"
 
+import * as styles from "../styles/layout.module.scss"
+
 const IndexPage = () => (
-  <>
-    <h1>hello, my name is matt</h1>
-    <p>let's build some cool stuff</p>
+  <div className={styles.mainContainer}>
+    <h1>openbiotics</h1>
+    <p>
+      <i>open source hardware for lab automation.</i>
+    </p>
 
     <ul>
       <li>
-        <Link to="/final">final project: liquid handling system</Link>
+        <Link to="/pcr">polymerase chain reaction</Link>
       </li>
       <li>
-        <Link to="/wk0">week 0: computer-aided design</Link>
-      </li>
-      <li>
-        <Link to="/wk1">week 1: computer-controlled cutting, git</Link>
-      </li>
-      <li>
-        <Link to="/wk2">week 2: electronics production</Link>
-      </li>
-      <li>
-        <Link to="/wk3">week 3: 3d printing and scanning</Link>
-      </li>
-      <li>
-        <Link to="/wk4">week 4: electronics production</Link>
-      </li>
-      <li>
-        <Link to="/todo">todo: things i want to try</Link>
-      </li>
-      <li>
-        <Link to="/commands">useful commands</Link>
-      </li>
-      <li>
-        <Link to="/test">test page</Link>
+        <Link to="/insulin">open source insulin</Link>
       </li>
     </ul>
-
-    <h2>lecture notes</h2>
-    <ul>
-      <li>
-        <Link to="/lec4">lecture 4: electronics design</Link>
-      </li>
-    </ul>
-
-    <h2>how to make</h2>
-    <ul>
-      <li>
-        <Link to="/building">how to make (almost) anything</Link>
-      </li>
-    </ul>
-  </>
+  </div>
 )
 
 export default IndexPage
